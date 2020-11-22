d35e51cd754f6a141a1069ef349a1b18ca4ef1ab
Pohl Comic <pcomic@ox-h3.msk.21-school.ru>
2020-11-22 20:40:24 +0300

    +sort

--- src/main.c
+++ src/main.c
@@ -1,5 +1,72 @@
 #include "ft_ls.h"
 
+int		asc_sort(t_file *f1, t_file *f2)
+{
+	return(f1->name < f2->name);
+}
+
+int		desc_sort(t_file *f1, t_file *f2)
+{
+	return(f1->name > f2->name);
+}
+
+t_file  *merge(t_file* l1, t_file* l2, int	(*f_ptr)(t_file *, t_file *))
+{
+	t_file dummy[1];
+	t_file* p = dummy;
+
+	while (l1 && l2)
+	{
+		// if (l1->name < l2->name)
+		if (f_ptr(l1, l2))
+		{
+			p->next = l1;
+			l1 = l1->next;
+		}
+		else {
+			p->next = l2;
+			l2 = l2->next;
+		}
+
+		p = p->next;
+	}
+
+	if (l1)
+		p->next = l1;
+	if (l2)
+		p->next = l2;
+
+	return dummy->next;
+}
+
+t_file* merge_Sort(t_file* head, int (*f_ptr)(t_file *, t_file *))
+{
+
+	/* terminating condition */
+
+	if (head == NULL || head->next == NULL)
+		return head;
+
+
+	t_file* slowPtr = head, *fastPtr = head, *prev = NULL;
+
+
+	while (fastPtr != NULL && fastPtr->next != NULL)
+	{
+		prev = slowPtr;
+		slowPtr = slowPtr->next;
+		fastPtr  = fastPtr->next->next;
+	}
+
+	/* split the list int two half */
+	prev->next = NULL;
+
+	t_file* l1 = merge_Sort(head, f_ptr);
+	t_file* l2 = merge_Sort(slowPtr, f_ptr);
+
+	return merge(l1, l2, f_ptr);
+}
+
 void        print_files(void)
 {
     printf("PRINT FILES\n");
@@ -40,7 +107,8 @@ void				print_dir_contains(t_file *d_fl, char *old_path)
 	t_file			*fls_copy;
 	char			new_path[PATH_MAX];
 	char			full_path[PATH_MAX + NAME_MAX];
-	
+	int				(*f_ptr)(t_file *, t_file *);
+
 	set_new_path(d_fl, new_path, old_path);
 	ft_printf("{green}%s:\n{eoc}", new_path);
 	fls = NULL;
@@ -65,6 +133,19 @@ void				print_dir_contains(t_file *d_fl, char *old_path)
 			exit(200);
 	}
 	format_print_files(fls);
+	ft_printf("\n");
+	ft_printf("\n");
+	ft_printf("\n");
+	f_ptr = asc_sort;
+	fls = merge_Sort(fls, f_ptr);
+	format_print_files(fls);
+	ft_printf("\n");
+	ft_printf("\n");
+	ft_printf("\n");
+	f_ptr = desc_sort;
+	fls = merge_Sort(fls, f_ptr);
+	format_print_files(fls);
+	
 	fls_copy = fls;
 	if (update_flags(-1) & 1 << fl_R)
 	{
